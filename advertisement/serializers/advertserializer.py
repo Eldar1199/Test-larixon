0576fa4cee3c63800111d62e8c4a8e8478481cdb
Eldar1199 <usenov1199@gmail.com>
2025-03-20 23:10:16 +0600

    add city_name and category_name

--- advertisement/serializers/advertserializer.py
+++ advertisement/serializers/advertserializer.py
@@ -5,6 +5,8 @@ from advertisement.models.advert import Advert
 
 
 class AdvertSerializer(serializers.ModelSerializer):
+    city_name = serializers.CharField(source='city.name', read_only=True)
+    category_name = serializers.CharField(source='category.name', read_only=True)
     class Meta:
         model = Advert
-        fields = '__all__'
+        fields = ['id', 'created', 'title', 'desc', 'city_name', 'category_name', 'views']
